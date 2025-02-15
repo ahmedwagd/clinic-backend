bff6d31976bf621deb35a2b26284b2ae5cc8db6c
Ahmed Wagdy <ahmedwa122@gmail.com>
2025-02-15 03:17:55 +0200

    initial

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { port } from 'utils/constants';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+}
+bootstrap();
---
+++ utils/constants.ts
@@ -0,0 +1,13 @@
+export const port = process.env.APP_PORT ?? 8080;
+export const frontendUrl = process.env.FRONTEND_URL;
+
+export const jwtSecret = process.env.JWT_SECRET;
+export const jwtExpIn = process.env.JWT_EXP_IN;
+export const jwtRefreshSecret = process.env.JWT_REFRESH_SECRET;
+export const jwtRefreshExpIn = process.env.JWT_REFRESH_EXP_IN;
+
+export const databaseHost = process.env.POSTGRES_HOST;
+export const databaseUser = process.env.POSTGRES_USER;
+export const databasePassword = process.env.POSTGRES_PASSWORD;
+export const databaseName = process.env.POSTGRES_DB;
+export const databaseUrl = process.env.DATABASE_URL;
